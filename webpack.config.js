0a5c93046e05d58f5c0665c95099511793f05e26
jorge.graca <jorgericardograca@gmail.com>
2018-06-08 00:04:10 +0100

    Added webpack server and vs configs

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,8 @@ module.exports = {
     mode: 'development',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        devtoolModuleFilenameTemplate: '[absolute-resource-path]'
     },
     module: {
         rules: [
